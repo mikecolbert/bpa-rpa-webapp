3299212bf64612c0fedf721db443c72cb2d51cb3
Mike Colbert <michael-colbert@uiowa.edu>
2021-11-11 08:11:08 -0600

    saving and displaying data in a python list

--- app.py
+++ app.py
@@ -1,15 +1,27 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, request, render_template, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html', pageTitle='Flask Server Home Page')
+contact_list = ['test tester']
 
-@app.route('/mike')
-def mike():
-    return render_template('mike.html', pageTitle='About Mike')
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html', pageTitle='BPA RPA Assignment',  contacts = contact_list)
+
+@app.route('/add', methods=['POST'])
+def add():
+    print('in add')
+    if request.method == 'POST':
+        print("inside post")
+        print(contact_list)
+        form = request.form
+        name = form['my_name']
+        print(name)
+        contact_list.append(name)
+        print(contact_list)
+        return redirect(url_for('index'))
+        #return render_template('index.html', pageTitle='BPA RPA Assignment', contacts = contact_list) 
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
